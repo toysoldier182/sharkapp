64555a57f1520d6f6e4d83fa1c10c5cdb5e72abc
juancarlosasensio <jasensio@ufm.edu>
2020-03-19 14:24:17 +0000

    Add Stimulus configuration to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,10 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
